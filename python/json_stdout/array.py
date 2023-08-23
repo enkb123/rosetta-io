c965565681c7896c2acc29c72130b0dbbb2e3a05
Kinnich <ardnik@gmail.com>
2023-08-23 09:52:26 -0700

    array and numbers scripts

---
+++ python/json_stdout/array.py
@@ -0,0 +1,5 @@
+import json
+import sys
+
+my_array = sys.argv[1:]
+print(json.dumps(my_array))
---
+++ python/json_stdout/object.py
@@ -0,0 +1,6 @@
+import json
+import sys
+from dataclasses import dataclass
+
+
+my_array = sys.argv[1:]
