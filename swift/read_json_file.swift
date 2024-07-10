bbb582cb69df1e3f6c8a8bd93fd4d1144694a44e
enkb123 <60800120+enkb123@users.noreply.github.com>
2024-07-10 11:33:14 -0400

    read_json edits

--- swift/read_json_file.swift
+++ swift/read_json_file.swift
@@ -8,11 +8,11 @@ guard let jsonFile = CommandLine.arguments.dropFirst().first else {
 
 let fileURL = URL(fileURLWithPath: jsonFile)
 let jsonData = try Data(contentsOf: fileURL)
-let people = try JSONSerialization.jsonObject(with: jsonData) as? [[String: Any]] ?? []
+let people = try JSONSerialization.jsonObject(with: jsonData) as! [[String: Any]]
 
-for (index, person) in people.enumerated() {
-    if let age = person["age"] as? Int, let firstName = person["first_name"] as? String {
-        print("Hello, \(age) year old \(firstName)")
-    }
+for (_, person) in people.enumerated() {
+    let age = person["age"] as! Int
+    let firstName = person["first_name"] as! String
+
+    print("Hello, \(age) year old \(firstName)")
 }
-
