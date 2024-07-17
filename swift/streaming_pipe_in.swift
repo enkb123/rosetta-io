820d2494bcfde1ab55b4fe0fe71f7ac09c1fd2af
Mike Nicholaides <mike@nicholaides.com>
2024-07-17 10:35:23 -0400

    Simplify streaming_pipe_in.swift and make it work more reliably
    Co-authored-by: enkb123 <60800120+enkb123@users.noreply.github.com>

--- swift/streaming_pipe_in.swift
+++ swift/streaming_pipe_in.swift
@@ -11,25 +11,8 @@ let arguments = CommandLine.arguments
 
 let pipe_in = arguments[1]
 
-guard let inputStream = InputStream(fileAtPath: pipe_in) else {
-    fatalError("Cannot open input stream")
+let fileHandle = FileHandle(forReadingAtPath: pipe_in)!
+
+for try await line in fileHandle.bytes.lines {
+    print(line.uppercased())
 }
-
-inputStream.open()
-
-let bufferSize = 1024
-let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
-
-while true {
-    let bytesRead = inputStream.read(buffer, maxLength: bufferSize)
-
-    let inputString = String(bytesNoCopy: buffer, length: bytesRead, encoding: .utf8, freeWhenDone: false)
-
-    let uppercaseString = inputString?.uppercased() ?? ""
-
-    print(uppercaseString, terminator: "")
-
-}
-
-inputStream.close()
-buffer.deallocate()
