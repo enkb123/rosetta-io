e0018051c67311e7e714fd9f9e63b974532b67b6
enkb123 <60800120+enkb123@users.noreply.github.com>
2024-07-30 10:57:53 -0400

    rm vendor

---
+++ rust/target/debug/deps/main-0883b25f59d454c3.d
@@ -0,0 +1,5 @@
+/Users/admin/Code/rosetta-io/rust/target/debug/deps/libmain-0883b25f59d454c3.rmeta: _main.rs
+
+/Users/admin/Code/rosetta-io/rust/target/debug/deps/main-0883b25f59d454c3.d: _main.rs
+
+_main.rs:
---
+++ rust/target/debug/deps/main-7ae54978a691642b.d
@@ -0,0 +1,5 @@
+/Users/admin/Code/rosetta-io/rust/target/debug/deps/libmain-7ae54978a691642b.rmeta: _main.rs
+
+/Users/admin/Code/rosetta-io/rust/target/debug/deps/main-7ae54978a691642b.d: _main.rs
+
+_main.rs:
