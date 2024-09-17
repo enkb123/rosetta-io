078f15446ae440ce3e15f2985b44bd871f686b71
Mike Nicholaides <mike@nicholaides.com>
2024-09-17 10:34:16 -0400

    Implement bash3 & bash5 "arguments" scripts

--- bash3/arguments.sh
+++ bash3/arguments.sh
@@ -1,8 +1,2 @@
-file_path="$1"
-
-if [ -z "$file_path" ]; then
-  echo "Usage: $0 <file_path>"
-  exit 1
-fi
-
-tr '[:upper:]' '[:lower:]' <<< "$file_path"
+echo "1st argument: $1"
+echo "2nd argument: $2"
