8b84a635a5cc5d40fce1dacb6f85e9c8cf5ad756
Mike Nicholaides <mike@nicholaides.com>
2024-09-17 10:33:57 -0400

    Polish "arguments" scripts and make the test case more lienient wrt leading & trailing writespace on each line

--- deno/arguments.mjs
+++ deno/arguments.mjs
@@ -1,2 +1,2 @@
-console.log("1st argument: " + Deno.args[0])
-console.log("2nd argument: " + Deno.args[1])
+console.log("1st argument:", Deno.args[0])
+console.log("2nd argument:", Deno.args[1])
--- r/arguments.R
+++ r/arguments.R
@@ -1,4 +1,4 @@
 args <- commandArgs(trailingOnly = TRUE)
 
-cat(paste0("1st argument: ", args[1],"\n"))
-cat(paste0("2nd argument: ", args[2],"\n"))
+cat("1st argument:", args[1], "\n")
+cat("2nd argument:", args[2], "\n")
