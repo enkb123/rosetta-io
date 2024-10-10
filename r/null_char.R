c8d5feadec214945a93ffb0e89014b28acc79af2
Mike Nicholaides <mike@nicholaides.com>
2024-10-10 13:42:48 -0400

    Remove the R null_char implementation and replace with note

--- r/null_char.R
+++
@@ -1,7 +0,0 @@
-temp_file <- tempfile()
-
-writeBin(c(charToRaw("Hello World "), as.raw(0), charToRaw("\n")), temp_file)
-
-system(paste("cat", temp_file))
-
-unlink(temp_file)
---
+++ r/null_char.R.md
@@ -0,0 +1,1 @@
+Strings in R cannot contain null characters (even though null characters are valid in UTF-8).
